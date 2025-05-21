fb73153331e9c3655fd1f7e0b1ad8ff10381f5be
Freyja <freyamarika@gmail.com>
2025-05-22 00:41:03 +1000

    add release and debug directories

--- main.cpp
+++ main.cpp
@@ -6,8 +6,10 @@ int main() {
     
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(PURPLE);
         DrawText("Welcome to Raylib!", 190, 200, 20, LIGHTGRAY);
+        DrawRectangle(200, 150, 200, 100, BLUE);
+        DrawCircle(400, 300, 50, RED);
         EndDrawing();
     }
     
